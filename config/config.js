e61d9a26f93d1f7406c0056b46e388bb61932991
ShubhamLakhotia-ui <lakhotia.sh@northeastern.edu>
2024-10-10 02:24:35 -0400

    add env vars

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: process.env.DB_DIALECT || "mysql",
   },
   test: {
     username: process.env.DB_USERNAME,
